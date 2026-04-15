cef7fa540b1a80a4788afdaf688c630869649ae0
abinash1417 <chandrannash281418@gmail.com>
2026-04-15 15:05:36 +0530

    chore: add pom.xml, main application entry point, and sample properties config

---
+++ backend/src/main/java/com/electromart/backend/ElectromartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.electromart.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectromartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectromartBackendApplication.class, args);
+	}
+
+}
