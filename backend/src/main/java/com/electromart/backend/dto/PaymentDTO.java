42101d1bfbbfecd4f742305bd8a93f12fd72c664
abinash1417 <chandrannash281418@gmail.com>
2026-04-18 09:50:51 +0530

    feat(backend): add PaymentDTO for payment data transfer

---
+++ backend/src/main/java/com/electromart/backend/dto/PaymentDTO.java
@@ -0,0 +1,60 @@
+package com.electromart.backend.dto;
+
+public class PaymentDTO {
+
+    private Long orderId;
+    private String paymentMethod;
+    private String cardNumber;
+    private String cardHolderName;
+    private String expiryDate;
+    private Double amount;
+
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getCardHolderName() {
+        return cardHolderName;
+    }
+
+    public void setCardHolderName(String cardHolderName) {
+        this.cardHolderName = cardHolderName;
+    }
+
+    public String getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(String expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+}
