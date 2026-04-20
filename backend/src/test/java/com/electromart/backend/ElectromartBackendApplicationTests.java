3e2116acc2cdbf798bd400d845f4d41db98a0ec6
abinash1417 <chandrannash281418@gmail.com>
2026-04-20 07:13:04 +0530

    test: add Spring Boot application test class

---
+++ backend/src/test/java/com/electromart/backend/ElectromartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.electromart.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectromartBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
