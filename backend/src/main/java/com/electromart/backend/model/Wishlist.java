b8d32e563f2ccbc2af563c5b2769c96ca6ac094a
abinash1417 <chandrannash281418@gmail.com>
2026-05-09 09:57:40 +0530

    feat(backend): add Wishlist entity model

---
+++ backend/src/main/java/com/electromart/backend/model/Wishlist.java
@@ -0,0 +1,26 @@
+package com.electromart.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "wishlists")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Wishlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+}
